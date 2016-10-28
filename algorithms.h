bce419de52c6e2c15c4eb909035c1fcb1f2c3ba9
Ante Žužul <ante.zuzul93@gmail.com>
2016-10-28 22:48:00 +0200

    Napisan algoritam 1. Testiranje nije provedeno. Testiranje ostavljam za sutra

---
+++ algorithms.h
@@ -0,0 +1,10 @@
+//
+// Created by ante on 10/28/16.
+//
+
+#ifndef LCPA_BWT_ALGORITHMS_H
+#define LCPA_BWT_ALGORITHMS_H
+
+std::list<auto> getIntervals(int i, int j);
+
+#endif //LCPA_BWT_ALGORITHMS_H
